8bf6be51b2c7db64a5855d4aacc83e610702da39
Almog-David <almog.david99@gmail.com>
2022-01-02 13:38:21 +0200

    add tests

---
+++ src/GraphTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getNodes() {
+    }
+
+    @Test
+    void setNodes() {
+    }
+
+    @Test
+    void getOut_edges() {
+    }
+
+    @Test
+    void setOut_edges() {
+    }
+
+    @Test
+    void getIn_edges() {
+    }
+
+    @Test
+    void setIn_edges() {
+    }
+
+    @Test
+    void getCounter() {
+    }
+
+    @Test
+    void setCounter() {
+    }
+
+    @Test
+    void getNode() {
+    }
+
+    @Test
+    void getEdge() {
+    }
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void addEdge() {
+    }
+
+    @Test
+    void removeNode() {
+    }
+
+    @Test
+    void removeEdge() {
+    }
+}
--- src/StudentCode.java
+++ src/StudentCode.java
@@ -3,7 +3,7 @@
  * A trivial example for starting the server and running all needed commands
  */
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class StudentCode {
     public static void main(String[] args) {
@@ -15,12 +15,15 @@ public class StudentCode {
         }
         String graphStr = client.getGraph();
         System.out.println(graphStr); // we need to upload the string graph in order to create a new graph
-        GraphAlgo g = load(graphStr);
+        GraphAlgo graph = load(graphStr); // i added in order to save the graph.
         client.addAgent("{\"id\":0}");
         String agentsStr = client.getAgents();
         System.out.println(agentsStr); // we need to upload the string of the agents and save it in some kind of list
+        Agent [] agents = load(agentsStr); // i added in order to save the agents. array because we don't add an agent in the middle
         String pokemonsStr = client.getPokemons();
         System.out.println(pokemonsStr); // the same in here
+        Queue<Pokemon> pokemons = new PriorityQueue<>((v1, v2) -> (int) (v1.getValue() - v2.getValue()));
+        pokemons = load(pokemonsStr); // add the pokemons to a queue because every time we need to add and remove the pokemon if we reach to him
         String isRunningStr = client.isRunning();
         System.out.println(isRunningStr);
         // BEFORE THE START WE NEED TO ALLOCATE THE AGENTS IN A POSITION
@@ -40,7 +43,34 @@ public class StudentCode {
 
         }
 
+
     }
+
+    public void CatchPokemon(){
+        List path;
+        double min = Double.MAX_VALUE;
+        int AgentID;
+        for (Pokemon p : pokemons){
+            int[] P_pos = p.findEdge(graph);
+            if(p.isCaptured()==false){
+                for(Agent a : agents){
+                    if (a.getSource()==-1){
+                       List<Integer> l = graph.shortestPath(a.getSource(),P_pos[0]);
+                       double temp = graph.calculateLength(l)/a.getSpeed();
+                       if(temp<min){
+                           min = temp;
+                           AgentID = a.getId();
+                           path = l;
+                       }
+                    }
+                }
+                path = path.add(p_pos[1]);
+                agents.get(AgentID).setPath(path);
+                p.setCaptured(true);
+            }
+        }
+    }
+
 
 
 
